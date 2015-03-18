f91ab8de25d52e62316a9d1215e22aaec9d51829
Sarun Wongpatcharapakorn <artwork.th@gmail.com>
2015-03-18 17:23:23 +0700

    Add empty SWInfiniteScrollView class

---
+++ Example/Pods/Headers/Public/SWPageViewController/SWInfiniteScrollView.h
@@ -0,0 +1,1 @@
+../../../../../Pod/Classes/SWInfiniteScrollView.h
---
+++ Pod/Classes/SWInfiniteScrollView.m
@@ -0,0 +1,21 @@
+//
+//  SWInfiniteScrollView.m
+//  Pods
+//
+//  Created by Sarun Wongpatcharapakorn on 3/18/15.
+//
+//
+
+#import "SWInfiniteScrollView.h"
+
+@implementation SWInfiniteScrollView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
